f76a5b08ffcee40e71b55a96d0a69abac445fde5
sohaghosen <sohag.hosen@outlook.com>
2021-11-12 01:32:34 +0600

    update api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
+const ObjectId = require("mongodb").ObjectId;
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -17,14 +18,66 @@ async function run() {
     await client.connect();
     const database = client.db(process.env.DB_NAME);
     const bikesCollection = database.collection("bikes");
-    app.get("/bikes", async (req, res) => {
 
+    // get all bikes 
+    app.get("/bikes", async (req, res) => {
       const result = await bikesCollection.find({}).toArray()
       res.send(result);
     })
 
-  } finally {
+    // get single bike
+    app.get("/bikes/:id", async (req, res) => {
+      const query = { _id: ObjectId(req.params.id) };
+      const result = await bikesCollection.findOne(query)
+      res.send(result);
+    })
+
+
+    // booking room
+    app.post("/room/booking", async (req, res) => {
+      const result = await bookings.insertOne(req.body);
+      res.json(result);
+    });
+
+    // get user booking rooms
+    app.get("/user/bookings/:email", async (req, res) => {
+      const query = { email: { $in: [req.params.email] } };
+      const result = await bookings.find(query).toArray();
+      res.send(result);
+    });
+
+    // delete booking room
+    app.delete("/booking/cancel/:id", async (req, res) => {
+      const query = { _id: ObjectId(req.params.id) };
+      const result = await bookings.deleteOne(query);
+      res.json(result);
+    });
+    // get all bookings
+    app.get("/bookings", async (req, res) => {
+      const result = await bookings.find({}).toArray();
+      res.send(result);
+    });
+
+    // insert new room
+    app.post("/room/new", async (req, res) => {
+      const result = await rooms.insertOne(req.body);
+      res.json(result);
+    });
+
+    // update room status
+    app.patch("/booking/status/:id", async (req, res) => {
+      console.log(req.params.id);
+      const filter = { _id: ObjectId(req.params.id) };
+      const options = { upsert: true };
+      // create a document that sets the plot of the movie
+      const updateDoc = {
+        $set: req.body,
+      };
+      const result = await bookings.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
   }
+  finally { }
 }
 run().catch(console.dir);
 
